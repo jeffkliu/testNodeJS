e24bbf25cd3c83f3ac2db968ebffad195cab9ba2
Jeff L <jeffreyliu0@gmail.com>
2018-07-08 12:57:12 -0700

    weather app added yargs

--- weather-app/playground/app.js
+++ weather-app/playground/app.js
@@ -1,9 +1,21 @@
-const request = require('request')
+const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs.options({
+	a: {
+		demand: true,
+		alias: 'address',
+		describe: 'Address to fetch weather for',
+		string: true
+	}
+})
 
 
 request({
 	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20phildelphia&key=AIzaSyA-7KlkAohBN4oOZUpPCtGxWz0JCtTI7BQ',
 	json: true
 }, (error, response, body)=> {
-	console.log(JSON.stringify(response, undefined, 2));
+	console.log(`Address: ${body.results[0].formatted_address}`);
+	console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+	console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 })
